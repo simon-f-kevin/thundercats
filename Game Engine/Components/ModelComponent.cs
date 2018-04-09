23b55fd9b44b7711f3bf3afa681b3717f917f6ed
xXDragonZlayerXx <s153298@student.hb.se>
2018-04-09 13:47:45 +0200

    Added a few components for rendering, changed a folders name and namespace to plural. Markus helped. A little bit. Very little. Almost everything.

--- Game Engine/Components/ModelComponent.cs
+++ Game Engine/Components/ModelComponent.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Game_Engine.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Game_Engine.Components
 {
     public class ModelComponent : Component
     {
         public Model Model { get; set; }
-
-        public ModelComponent(Model model) : base()
+        
+        public ModelComponent(Entity id, Model model) : base(id)
         {
             Model = model;
         }
---
+++ Game Engine/Components/TransformComponent.cs
@@ -0,0 +1,22 @@
+﻿using Game_Engine.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Engine.Components
+{
+    public class TransformComponent : Component
+    {
+        public Vector3 position;
+        public Vector3 scale;
+        public Matrix rotationMatrix { get; set; }
+
+        TransformComponent(Entity id) : base(id)
+        {
+
+        }
+    }
+}
