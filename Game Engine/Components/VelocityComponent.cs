babe447a8bc7eddf9b142c2c9a5065066ab11a90
xXDragonZlayerXx <s153298@student.hb.se>
2018-04-10 15:19:14 +0200

    VelocityComponent and basic movement system

---
+++ Game Engine/Components/VelocityComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game_Engine.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Game_Engine.Components
+{
+    public class VelocityComponent : Component
+    {
+        public Vector3 Velocity;
+        public Vector3 DefaultSpeed;
+
+        public VelocityComponent(Entity id) : base(id)
+        {
+            
+        }
+        public VelocityComponent(Entity id, Vector3 velocity, Vector3 DefaultSpeed) : base(id)
+        {
+            this.Velocity = velocity;
+            this.DefaultSpeed = DefaultSpeed;
+
+        }
+    }
+}
---
+++ Game Engine/Systems/MovementSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game_Engine.Components;
+using Game_Engine.Managers;
+using Microsoft.Xna.Framework;
+
+namespace Game_Engine.Systems
+{
+    class MovementSystem : IUpdateableSystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            var velocityComponents = ComponentManager.Instance.GetComponentDictionary<VelocityComponent>();
+            foreach(var velocityComponent in velocityComponents)
+            {
+                var velocity = velocityComponent.Value as VelocityComponent;
+                var transformation = ComponentManager.Instance.GetComponentOfEntity<TransformComponent>(velocityComponent.Key);
+
+                transformation.Position += velocity.Velocity;
+            }
+
+        }
+    }
+}
