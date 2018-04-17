eaed5fa1c04244b78c366d789431e4b008a9463d
Elvir Dzeko <dzekson94@gmail.com>
2018-04-17 11:35:36 +0200

    UI render

---
+++ thundercats/Systems/UIRenderSystem.cs
@@ -0,0 +1,48 @@
+﻿using Game_Engine.Components;
+using Game_Engine.Managers;
+using Game_Engine.Systems;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thundercats.Systems
+{
+    public class UIRenderSystem : IDrawableSystem
+    {
+        private SpriteBatch sb;
+
+        public void Initialize(SpriteBatch spritebatch)
+        {
+            this.sb = spritebatch;
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            var components = ComponentManager.Instance.GetComponentDictionary<UIComponent>();
+
+            foreach (var component in components.Values)
+            {
+                var c = component as UIComponent;
+
+                if (c.UITexture != null)
+                    sb.DrawString(c.SpriteFont, c.Text, c.Position, c.Color);
+                else if (c.Text != null)
+                    DrawText(c);
+            }   
+        }
+
+        private void DrawText(UIComponent uIComponent)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DrawTexture(UIComponent uIComponent)
+        {
+            
+        }
+    }
+}
