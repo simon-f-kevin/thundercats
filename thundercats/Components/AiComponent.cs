a46a45544003cc6f487a9b892ffd470518d3fad9
Mackan95bowling <Markus@furuliden.com>
2018-04-26 13:51:18 +0200

    Started with AI, component and system files created

---
+++ thundercats/Components/AiComponent.cs
@@ -0,0 +1,19 @@
+﻿using Game_Engine.Components;
+using Game_Engine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thundercats.Components
+{
+    public class AiComponent : Component
+    {
+        public enum AiState {Losing,Winning,Even};
+        public AiComponent(Entity id) : base(id)
+        {
+
+        }
+    }
+}
---
+++ thundercats/Systems/AiSystem.cs
@@ -0,0 +1,24 @@
+﻿using Game_Engine.Systems;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace thundercats.Systems
+{
+    public class AiSystem : IUpdateableSystem
+    {
+        /* Check if "Lane" is clear
+         * Check NextRow if winning, dont care for powerups only survival!
+         * if! losing! the AI should check after powerups! 
+         * in Nextrow we need to evaluate where to go!
+         */
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
