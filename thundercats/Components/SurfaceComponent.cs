57f3e05009faa25602e97a9d73bf6de9442beb8b
Mackan95bowling <Markus@furuliden.com>
2018-04-27 15:10:09 +0200

    Changd Surface to right values

--- thundercats/Components/SurfaceComponent.cs
+++ thundercats/Components/SurfaceComponent.cs
@@ -10,7 +10,7 @@ namespace thundercats.Components
 {
     public class SurfaceComponent : Component
     {
-        public enum Surface { Fast = -20, Standard = 0, Slow = 80 };
+        public enum Surface { Fast = 80, Standard = 0, Slow = -30 };
         public Surface SurfaceType {get; set;}
         public SurfaceComponent(Entity id, Surface surfaceType) : base(id)
         {
