fb894bc10c2753b8226d34b014308ef747f50b70
Mackan95bowling <Markus@furuliden.com>
2018-05-08 14:59:09 +0200

    FrameWork of how we think we should do AI! IF anyone wana help u r welcome.

--- thundercats/GameStates/States/AiStates/Winning.cs
+++ thundercats/GameStates/States/AiStates/Winning.cs
@@ -12,20 +12,81 @@ namespace thundercats.GameStates.States.AiStates
     public class Winning : IAiState
     {
         private AiStateManager aiStateManager;
+
         private AiSystem aiSystem;
+
         private int TargetValue { get; set; }
+
+        private int[,] WorldMatrix { get; set; }
+
+        private Random random;
+
         public Winning(AiStateManager aiStateManager)
         {
             this.aiStateManager = aiStateManager;
-            TargetValue = 1;
+            random = new Random();
+            TargetValue = random.Next(2, 4);
+        }
+        public void Evaluate()
+        {
+
         }
         public void Update(GameTime gameTime)
         {
-            //jag vill väl bara uppdatera AiSystem? så kör allt i AI?
-            aiSystem.CheckNextRow(/*fuckit*/,TargetValue);
-           // aiSystem.CheckNextRow(,TargetValue);
-            
+            SystemManager.Instance.Update(gameTime);
+
+            CalculateMove();
+            aiSystem.CheckNextRow(/*FUUCK*/, TargetValue);
+        }
+
+        private void CalculateMove()
+        {
+            // Player pos row should be updated depending on real position. (transformComponent)
+            Point playerPosRow = new Point(1, 4); // what row player should be on in matrix
+            int[] nextMatrixRow = GetRow(WorldMatrix, playerPosRow.X); // get next row in front of player
+
+            var currentBlock = GetCurrentBlock(playerPosRow);
+            Point decision = ChooseBlock(nextMatrixRow, playerPosRow.X);
+            var nextBlock = GetNextRowBlock(decision);
+            // Check What decision to do
+
+            MakeMove(currentBlock, nextBlock);
+        }
+
+        private void MakeMove(Vector3 currentBlock, Vector3 nextBlock)
+        {
             throw new NotImplementedException();
+        }
+
+        private Vector3 GetNextRowBlock(Point decision)
+        {
+            // return next block dimensions
+            return default(Vector3);
+        }
+
+        private Vector3 GetCurrentBlock(Point playerPosRow)
+        {
+            // return current block dimensions
+            return default(Vector3);
+        }
+
+        private int[] GetRow(int[,] worldMatrix, int row)
+        {
+            // should return row
+            return new int[] { worldMatrix[row + 1, 0], worldMatrix[row + 1, 1], worldMatrix[row + 1, 2] };
+        }
+
+        private Point ChooseBlock(int[] row, int RowIndex)
+        {
+            int currentChoice = 0;
+            // Choose the right block on that row
+            for (int i = 0; i < row.Length; i++)
+            {
+                // Logic should be here to choose column/block
+                if (row[i] == 1) return new Point(RowIndex, i);
+            }
+            return new Point(RowIndex, currentChoice);
+            // Index of the next block the ai is moving to;
         }
     }
 }
