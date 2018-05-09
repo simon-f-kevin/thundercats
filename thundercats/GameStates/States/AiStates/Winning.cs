a37d50ac795b06da022d0fd7a7b045dd9ebebc2d
Mackan95bowling <Markus@furuliden.com>
2018-05-09 07:32:01 +0200

    Added some small things to Winning state that we already had in losing

--- thundercats/GameStates/States/AiStates/Winning.cs
+++ thundercats/GameStates/States/AiStates/Winning.cs
@@ -36,7 +36,7 @@ namespace thundercats.GameStates.States.AiStates
             SystemManager.Instance.Update(gameTime);
 
             CalculateMove();
-            aiSystem.CheckNextRow(/*FUUCK*/, TargetValue);
+            //aiSystem.CheckNextRow(/*FUUCK*/ TargetValue);
         }
 
         private void CalculateMove()
@@ -54,8 +54,25 @@ namespace thundercats.GameStates.States.AiStates
         }
 
         private void MakeMove(Vector3 currentBlock, Vector3 nextBlock)
-        {
-            throw new NotImplementedException();
+        {  
+            //var VelocityComponent = ComponentManager.Instance.GetComponentOfEntity<VelocityComponent>(entity);
+            if (currentBlock.X > nextBlock.X)
+            {
+                //jump left
+                //PlayerActions.AcceleratePlayerLeftWards(VelocityComponent);
+                //PlayerActions.PlayerJumpSpeed(VelocityComponent);
+            }
+            if (currentBlock.X < nextBlock.X)
+            {
+                //jump Right
+                //PlayerActions.AcceleratePlayerRightwards(VelocityComponent);
+                //PlayerActions.PlayerJumpSpeed(VelocityComponent);
+            }
+            else
+            {
+                //Continue run
+                //PlayerActions.AcceleratePlayerForwards(VelocityComponent);
+            }
         }
 
         private Vector3 GetNextRowBlock(Point decision)
@@ -83,7 +100,8 @@ namespace thundercats.GameStates.States.AiStates
             for (int i = 0; i < row.Length; i++)
             {
                 // Logic should be here to choose column/block
-                if (row[i] == 1) return new Point(RowIndex, i);
+                if (row[i] != 0) return new Point(RowIndex, i); // this should be the only thing we need to do? cuz we just want to survive
+               
             }
             return new Point(RowIndex, currentChoice);
             // Index of the next block the ai is moving to;
