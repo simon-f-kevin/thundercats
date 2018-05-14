97d9b7dcb0e81c5401479eb69bdb21a70b3ecceb
zimme95 <zimme95@gmail.com>
2018-05-14 19:20:49 +0200

    fixed resharpers wrongdoings

--- thundercats/Game1.cs
+++ thundercats/Game1.cs
@@ -46,7 +46,7 @@ namespace thundercats
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.StartAudioManager will enumerate through any components
+        /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
         protected override void Initialize()
