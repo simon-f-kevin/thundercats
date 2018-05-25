f7e309f02485e51565d6d38f8c1f70457f829dbd
Elvir Dzeko <dzekson94@gmail.com>
2018-05-25 11:50:58 +0200

    refactored physicsystems

---
+++ Game_Engine/Systems/Physics/FrictionSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game_Engine.Components;
+using Game_Engine.Managers;
+using Microsoft.Xna.Framework;
+
+namespace Game_Engine.Systems
+{
+    public class FrictionSystem : IUpdateableSystem
+    {
+        public void Update(GameTime gameTime)
+        {
+            var velocityComponents = ComponentManager.Instance.GetConcurrentDictionary<VelocityComponent>();
+
+            Parallel.ForEach(velocityComponents, velocityComponent =>
+            {
+                var velocity = velocityComponent.Value as VelocityComponent;
+                var frictionComponent = ComponentManager.Instance.ConcurrentGetComponentOfEntity<FrictionComponent>(velocityComponent.Key);
+
+                if (frictionComponent != null)
+                {
+                    if (velocity != null && velocity.Velocity != Vector3.Zero)
+                    {
+                        velocity.Velocity.X *= frictionComponent.Friction;
+                        velocity.Velocity.Z *= frictionComponent.Friction;
+                    }
+
+                }
+            });
+
+        }
+    }
+}
